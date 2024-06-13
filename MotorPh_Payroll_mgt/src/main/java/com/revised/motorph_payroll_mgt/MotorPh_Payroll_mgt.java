42996083a75fe59e415486186bbe3606cf9c035d
rashni0997 <155600877+rashni0997@users.noreply.github.com>
2024-06-13 04:55:39 -0400

    Add files via upload

---
+++ MotorPh_Payroll_mgt/src/main/java/com/revised/motorph_payroll_mgt/MotorPh_Payroll_mgt.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.revised.motorph_payroll_mgt;
+
+/**
+ *
+ * @author rashni alicando
+ */
+public class MotorPh_Payroll_mgt {
+
+    public static void main(String[] args) {
+        
+        signin_page sign = new signin_page();
+        sign.setVisible(true);
+        sign.pack();
+        sign.setLocationRelativeto(null);
+        
+        
+        
+    }
+}
